0d89882f3cabb13e70064117b6e6946647e30393
Alexander <g.a.androidjc2@ya.ru>
2020-02-20 13:00:43 +0400

    agsjiked

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    Notification.requestPermission()
+        .then(permission => {
+            console.log(permission);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    document.getElementById("sub").onclick = function() {
+        subscribe();
+    }
+})
+
+function registerSW() {
     if ('serviceWorker' in navigator){
         navigator.serviceWorker.register('sw.js', {scope: "/"}).then(function(registration){
                 console.log('service worker registration succeeded:',registration);
@@ -10,8 +24,4 @@ document.addEventListener("DOMContentLoaded", function() {
     else{
         console.log('service workers are not supported.');
     }
-
-    document.getElementById("sub").onclick = function() {
-        subscribe();
-    }
-})
+}
