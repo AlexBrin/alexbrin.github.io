9cf3a7ceb5cb878bd860dffa115dc2c733890a89
Alexander <g.a.androidjc2@ya.ru>
2020-02-20 10:59:29 +0400

    sw

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA1cTRtYRpVmACeaqbdnHnruvoWEj_LLdM",
+    authDomain: "titan-checker-dev.firebaseapp.com",
+    databaseURL: "https://titan-checker-dev.firebaseio.com",
+    projectId: "titan-checker-dev",
+    storageBucket: "titan-checker-dev.appspot.com",
+    messagingSenderId: "468240947431",
+    appId: "1:468240947431:web:d77511f15ae3b6f433ea42",
+    measurementId: "G-WR3G7792E4"
+})
+firebase.analytics();
+
+var messaging = firebase.messaging();
+messaging.getToken().then(currentToken => {
+    if(currentToken) {
+        console.log("Token:", currentToken)
+    } else {
+        console.log('No Instance ID token available. Request permission to generate one.');
+    }
+})
+    .catch(err => {
+        console.error(err);
+    });
