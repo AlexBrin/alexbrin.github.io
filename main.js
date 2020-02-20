282d3e75d7abe961badacb3dbe277ad28c5cfa86
Alexander <g.a.androidjc2@ya.ru>
2020-02-20 12:47:55 +0400

    ok

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
 var messaging = firebase.messaging();
 
 document.addEventListener("DOMContentLoaded", function() {
--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
-
 firebase.initializeApp({
     apiKey: "AIzaSyA1cTRtYRpVmACeaqbdnHnruvoWEj_LLdM",
     authDomain: "titan-checker-dev.firebaseapp.com",
